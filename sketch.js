de5713e557dc5a96d6e0f614128d7aad3ff6c3f6
sanjuwillb <79291401+sanjuwillb@users.noreply.github.com>
2021-04-03 20:24:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,7 +56,7 @@ if (keyCode === LEFT_ARROW && packageSprite.y === 200){
 if (keyCode === RIGHT_ARROW && packageSprite.y === 200){
   Matter.Body.translate(packageBody, {x:+20,y:0})
 }
-  if (keyDown(DOWN_ARROW) && (packageBody.x > 400 || packageSprite.x < 400){
+  if (keyDown(DOWN_ARROW) && (packageBody.x > 400 || packageSprite.x < 400)){
 	Matter.Body.setStatic(packageBody,false)
 }
   if (packageBody.VelocityY>0){
